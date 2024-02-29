fec0e335bb2ef57793fa2f2b479175098acd8e84
Abdul Hadi <abdulandalas1234567@gmail.com>
2024-03-01 06:46:33 +0700

    Commit kelima : Pengumpulan Tugas 9 OOP PHP

--- OOP-PHP/index.php
+++ OOP-PHP/index.php
@@ -11,11 +11,15 @@ echo "Cold blooded: " . $sheep->cold_blooded  . "</br>". "</br>";
 $kodok = new Frog("buduk");
 echo "Name: " . $kodok->name . "</br>";
 echo "Legs: " . $kodok->legs  . "</br>";
-echo "Cold blooded: " . $kodok->cold_blooded  . "</br>". "</br>";
-$kodok->jump(); // "Jump: Hop Hop"
+echo "Cold blooded: " . $kodok->cold_blooded  . "</br>";
+echo "Jump: " ;
+echo $kodok->jump(); // "Jump: Hop Hop"
+echo "</br>"."</br>";
 
 $sungokong = new Ape("kera sakti");
 echo "Name: " . $sungokong->name . "</br>";
 echo "Legs: " . $sungokong->legs  . "</br>";
 echo "Cold blooded: " . $sungokong->cold_blooded  . "</br>";
-$sungokong->yell(); // "Yell: Auooo"
+echo "Yell: " ;
+echo $sungokong->yell(); // "Jump: Hop Hop"
+echo "</br>"."</br>";
